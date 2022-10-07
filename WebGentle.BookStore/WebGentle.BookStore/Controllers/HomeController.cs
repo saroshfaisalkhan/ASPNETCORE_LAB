9553f44a0ae04af455949487a252bfb8bcd45000
sarosh <sarosh@DESKTOP-LN51Q13>
2022-10-07 03:33:14 -0700

    Just Now Changes

---
+++ WebGentle.BookStore/WebGentle.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebGentle.BookStore.Controllers
+{
+    public class HomeController:Controller
+    {
+
+        public string Index()
+        {
+            return "Web Gentle"; 
+        }
+    }
+}
